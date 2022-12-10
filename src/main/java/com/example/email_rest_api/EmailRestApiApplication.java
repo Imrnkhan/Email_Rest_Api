866104296d167936c0007515d1b4ea8dd2ee0b06
91905 <imrankhan7581@gmail.com>
2022-12-10 21:10:40 +0530

    Initial commit

---
+++ src/main/java/com/example/email_rest_api/EmailRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.email_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailRestApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/email_rest_api/Model/User.java
@@ -0,0 +1,4 @@
+package com.example.email_rest_api.Model;
+
+public class User {
+}
